3a99077a01ceb0acd211bb5700d95da5f73318df
Nils Montenegro <nils@montenegros.de>
2020-10-07 22:01:49 +0200

    save croped img of face

--- facedetect.py
+++ facedetect.py
@@ -5,8 +5,8 @@ import sys
 net = cv2.dnn.readNetFromCaffe("deploy.prototxt", "res10_300x300_ssd_iter_140000_fp16.caffemodel")
 
 def enlarge_rect(startX, startY, endX, endY, w, h):
-	lenX = (endX - startX) / 4
-	lenY = (endY - startY) / 4
+	lenX = (endX - startX) / 3
+	lenY = (endY - startY) / 3
 
 	startX = int(max(startX - lenX, 0))
 	startY = int(max(startY - lenY, 0))
@@ -35,20 +35,26 @@ def detect_face(filename):
 		detections_array.append((confidence, box))
 
 	print(detections_array[0])
-	#detections_array.sort()
 	detections_array = sorted(detections_array, key=lambda tup: tup[0])
 
-	for (confidence, box) in detections_array[-1:]:
-		(startX, startY, endX, endY) = box.astype("int")
-		(startX_large, startY_large, endX_large, endY_large) = enlarge_rect(startX, startY, endX, endY, w, h)
-		text = "{:.2f}%".format(confidence * 100)
-		y = startY - 10 if startY - 10 > 10 else startY + 10
-		cv2.rectangle(image, (startX, startY), (endX, endY),(0, 0, 255), 2)
-		cv2.rectangle(image, (startX_large, startY_large), (endX_large, endY_large),(0, 255, 0), 2)
-		cv2.putText(image, text, (startX, y),cv2.FONT_HERSHEY_SIMPLEX, 0.45, (0, 0, 255), 2)
+	(confidence, box) = detections_array[-1]
 
-	cv2.imshow("Output", image)
+	(startX, startY, endX, endY) = box.astype("int")
+	(startX_large, startY_large, endX_large, endY_large) = enlarge_rect(startX, startY, endX, endY, w, h)
+
+	img_copy = image.copy()[startY_large:endY_large, startX_large:endX_large]
+	cv2.imwrite("output/" + filename, img_copy)
+
+	text = "{:.2f}%".format(confidence * 100)
+	y = startY - 10 if startY - 10 > 10 else startY + 10
+	cv2.rectangle(image, (startX, startY), (endX, endY),(0, 0, 255), 2)
+	cv2.putText(image, text, (startX, y),cv2.FONT_HERSHEY_SIMPLEX, 0.45, (0, 0, 255), 2)
+
+	crop_img = image[startY_large:endY_large, startX_large:endX_large]
+
+	cv2.imshow("Output", crop_img)
 	cv2.waitKey(0)
+
 
 for filename in sys.argv[1:]:
 	print(filename)
