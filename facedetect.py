c9c6d958e72fcd126ec17ce6789c592452d2298f
Nils Montenegro <nils@montenegros.de>
2020-10-07 22:27:23 +0200

    select which img to save: press enter vs any other button

--- facedetect.py
+++ facedetect.py
@@ -34,7 +34,6 @@ def detect_face(filename):
 		
 		detections_array.append((confidence, box))
 
-	print(detections_array[0])
 	detections_array = sorted(detections_array, key=lambda tup: tup[0])
 
 	(confidence, box) = detections_array[-1]
@@ -43,7 +42,6 @@ def detect_face(filename):
 	(startX_large, startY_large, endX_large, endY_large) = enlarge_rect(startX, startY, endX, endY, w, h)
 
 	img_copy = image.copy()[startY_large:endY_large, startX_large:endX_large]
-	cv2.imwrite("output/" + filename, img_copy)
 
 	text = "{:.2f}%".format(confidence * 100)
 	y = startY - 10 if startY - 10 > 10 else startY + 10
@@ -53,10 +51,13 @@ def detect_face(filename):
 	crop_img = image[startY_large:endY_large, startX_large:endX_large]
 
 	cv2.imshow("Output", crop_img)
-	cv2.waitKey(0)
 
+	if cv2.waitKey(0) == 13: # enter key
+		cv2.imwrite("output/" + filename, img_copy)
+		print("took   " + str(confidence))
+	else:
+		print("                       discarded  " + str(confidence))
 
 for filename in sys.argv[1:]:
-	print(filename)
 	detect_face(filename)
 
